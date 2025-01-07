032f11ba1b63f3eefbdb73d6deb7dc796d6ab072
shemm1 <shemm1@EARL-BCT-237716.ad.ocdsb.ca>
2025-01-07 10:58:16 -0500

    First Commit, little meaningful code

---
+++ JavaApplication5/src/Upgrade.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author shemm1
+ */
+//import java.math.BigDecimal;
+
+public class Upgrade {
+   String name, iconPath, description;
+   double cost, multiplier;
+   boolean isBought;
+   
+   public Upgrade(String name, String iconPath, String Description, double cost){
+   }
+}
